4b2437b6c04e40b3a551b026a3d7fca6d3934323
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-06-24 12:16:06 -0600

    more vps

--- git_coin/dig.rb
+++ git_coin/dig.rb
@@ -7,21 +7,25 @@ class Dig
   POST = URI.parse("http://git-coin.turingapps.io/hash")
   def initialize(tag)
     @tag = tag
+    puts "miner got to init"
     run
   end
 
   def run
+    puts "miner got to run"
     key = (1..30000).to_a.sample.to_s + "Grace Hopper's Ghost"
     target = Net::HTTP.get(URI.parse("http://git-coin.turingapps.io/target")).hex
     loop do
       100_000_000_000.times do |i|
         sha = key + i.to_s
         if i % 1_000_000 == 0
+          puts "#{tag} at iteration #{i}"
           target = Net::HTTP.get(URI.parse("http://git-coin.turingapps.io/target"))
         end
         if Digest::SHA1.hexdigest(sha).hex < target.hex
-          Net::HTTP.post_form(POST, { "message" => sha, "owner" => "Hopper Posse 4 Lyfe" })
+          response = Net::HTTP.post_form(POST, { "message" => sha, "owner" => "Hopper Posse 4 Lyfe" })
           target = Net::HTTP.get(URI.parse("http://git-coin.turingapps.io/target"))
+          puts "#{sha} is just got #{response.body}"
           break
         end
       end
